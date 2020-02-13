d886ba7ea3d6eb0e137884995d4643169985100d
Aarushi <aarushiibisht@gmail.com>
2020-02-13 00:00:56 -0500

    docker file for post processing service

--- PostProcessing/post_processing.py
+++ PostProcessing/post_processing.py
@@ -1,28 +1,31 @@
 import json
 from kafka import KafkaConsumer, KafkaProducer
 import os
-
+import sys
 
 def process_data():
     consumer = KafkaConsumer('modelExecutionResult', bootstrap_servers=os.getenv('KAFKA'))
     producer = KafkaProducer(bootstrap_servers=os.getenv('KAFKA'))
     print("Post processing service started consuming!!")
+    sys.stdout.flush()
+    sys.stderr.flush()
     while True:
         for msg in consumer:
-            d = bytes.decode(msg.value())
+            d = bytes.decode(msg.value)
             result = compute(json.loads(d))
             print("Post processing service started producing!!")
             producer.send("postProcessingResult", result)
             producer.flush()
+            sys.stdout.flush()
+            sys.stderr.flush()
 
 
 def compute(d):
     # Assuming 4 members per family consuming 50 gallons per month
-
     d["post_processed"] = total_cost(d["modelResult"])
     d["status"] = "COMPLETED"
     print(d)
-    return bytes(d)
+    return json.dumps(d).encode()
 
 
 def total_cost(water):
@@ -31,6 +34,7 @@ def total_cost(water):
 
 
 if  __name__ == "__main__":
+    print("started post processing service!!")
     process_data()
 
 
