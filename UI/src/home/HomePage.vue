2124f304955fd55cac359e143618b90a3ed4f8be
Akhil Nagulavancha <akhil@149-160-165-45.dhcp-bl.indiana.edu>
2020-02-13 21:55:47 -0500

    First draft of UI

---
+++ UI/src/home/HomePage.vue
@@ -0,0 +1,83 @@
+<template>
+<div @keydown.enter="handleSubmit">
+<p>
+<router-link to="/view">View sessions</router-link>
+</p>
+      <br></br>
+      <p> <b>Please enter the Area of your House in square feet (Sqft)</b></p>
+Area:<br>
+<input v-model="Area" name="Area" placeholder="Enter Area">
+      <br></br>
+      <p> <b> Please select the months</b> </p>
+    <input type="checkbox" name="0" value="0" v-model="checkedNames">
+    <label for="0"> January</label><br>
+    <input type="checkbox" name="1" value="1" v-model="checkedNames">
+    <label for="1"> February</label><br>
+    <input type="checkbox" name="2" value="2" v-model="checkedNames">
+    <label for="2"> March</label><br>
+    <input type="checkbox" name="3" value="3" v-model="checkedNames">
+    <label for="3">April</label><br>
+    <input type="checkbox" name="4" value="4" v-model="checkedNames">
+    <label for="4">May</label><br>
+    <input type="checkbox" name="5" value="5" v-model="checkedNames">
+    <label for="5">June</label><br>
+    <input type="checkbox" name="6" value="6" v-model="checkedNames">
+    <label for="6">July</label><br>
+    <input type="checkbox" name="7" value="7" v-model="checkedNames">
+    <label for="7">August</label><br>
+    <input type="checkbox" name="8" value="8" v-model="checkedNames">
+    <label for="8">September</label><br>
+    <input type="checkbox" name="9" value="9" v-model="checkedNames">
+    <label for="9">October</label><br>
+    <input type="checkbox" name="10" value="10" v-model="checkedNames">
+    <label for="10">November</label><br>
+    <input type="checkbox" name="11" value="11" v-model="checkedNames">
+    <label for="11">December</label><br>
+    <p><b> Enter valid Zipcode </b></p>
+    <input v-model="zipcode" type="text" name="Area" placeholder="Zipcode">
+    <br></br>
+    <button @click="handleSubmit" >Submit</button>
+    <br></br>
+    <p>
+      <router-link to="/login">Logout</router-link>
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      Area:'',
+      checkedNames:[],
+      zipcode:''
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      console.log(this.Area)
+      console.log(this.checkedNames)
+      console.log(this.zipcode)
+      console.log(this.$store.state.authentication.user.username)
+
+      e.preventDefault();
+                    let currentObj = this;
+                    axios.post('http://localhost:7000/api/experiments/', {
+                        username:this.$store.state.authentication.user.username,
+                        house_area:this.Area,
+                        pincode: this.zipcode,
+                        months: this.checkedNames
+                    })
+                    .then(function (response) {
+                        currentObj.output = response.data;
+                    })
+                    .catch(function (error) {
+                        currentObj.output = error;
+                    });
+
+
+    }
+  }
+}
+</script>
