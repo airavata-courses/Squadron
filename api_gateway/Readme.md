e78b8684466e29f924787aed94230e225faeb9cd
Shivam Rastogi <shivam_riky@yahoo.com>
2020-02-14 08:42:34 +0530

    Fixed API gateway branching issues

---
+++ api_gateway/Readme.md
@@ -0,0 +1,6 @@
+# API Gateway
+
+Only this service is exposed to the front-end applications and all the requests from front-end are routed via API Gateway.
+
+## Install instructions
+1. Clone the repo. 
---
+++ api_gateway/api_gateway/api/serializers.py
@@ -0,0 +1,33 @@
+import jsonpickle
+from rest_framework import serializers
+from rest_framework.utils import json
+
+
+class Experiment(object):
+    def __init__(self, username, house_area, pincode, months, request_id=None, status=None):
+        self.username = username
+        self.house_area = house_area
+        self.pincode = pincode
+        self.months = months
+        self.request_id = request_id
+        self.status = status
+
+
+class ExperimentSerializer(serializers.Serializer):
+    request_id = serializers.CharField(required=False, allow_blank=True)
+    username = serializers.CharField(max_length=30, required=True)
+    house_area = serializers.IntegerField(required=True)
+    pincode = serializers.IntegerField(required=True)
+    months = serializers.ListField(required=True)
+    status = serializers.CharField(required=False, allow_blank=True)
+
+    def create(self, validated_data):
+        return Experiment(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.house_area = validated_data.get('house_area', instance.house_area)
+        instance.pincode = validated_data.get('pincode', instance.pincode)
+        instance.months = validated_data.get('months', instance.months)
+        instance.status = validated_data.get('status', instance.status)
+        return instance
+
