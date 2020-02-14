b0ab9d5992775d0497769801234747768f38304b
Akhil Nagulavancha <akhil@149-160-165-45.dhcp-bl.indiana.edu>
2020-02-13 19:47:57 -0500

    User Interface

---
+++ UI/src/view/ViewPage.vue
@@ -0,0 +1,47 @@
+<template>
+<div id="textExample">
+  <table class="table table-bordered">
+			<thead>
+				<tr>
+					<th> request Id</th>
+					<th> User Name </th>
+					<th> House Area </th>
+					<th> Months </th>
+					<th> Payment</th>
+					<th> houseArea</th>
+					<th> status</th>
+				</tr>
+			</thead>
+			<tr v-for="i in session[0]">
+				<td v-for="j in session">{{j[i]}}</td>
+			</tr>
+		</table>
+    <p>
+    <router-link to="/login">Logout</router-link>
+   </p>
+
+</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data () {
+			return {
+session:''
+            }
+		},
+    created () {
+			axios.get('http://localhost:9000')
+			.then(
+				response => {
+					this.session = response.body
+					// console.log(response)
+				})
+			.catch(function(error){
+				console.log(error)
+			})
+		}
+	}
+</script>
+<p>
+<router-link to="/view">View sessions</router-link>
+</p>
